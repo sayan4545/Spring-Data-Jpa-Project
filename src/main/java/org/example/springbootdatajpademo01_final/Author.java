3748500b45a7225383df3ea21026ebfec9f868d2
sayan4545 <sayanchatterjee3010@gmail.com>
2024-04-11 03:54:02 +0530

    Adding @Table and @Column annotations

--- src/main/java/org/example/springbootdatajpademo01_final/Author.java
+++ src/main/java/org/example/springbootdatajpademo01_final/Author.java
@@ -1,23 +1,41 @@
 package org.example.springbootdatajpademo01_final;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Entity
+@Table(
+        name = "AUTHOR_TBL"
+)
 @Data
 @NoArgsConstructor
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(
+            nullable = false,
+            length = 50
+    )
     private String first_Name;
     private String last_Name;
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
     private Integer age;
+    @Column(
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastupdatedAt;
 }
